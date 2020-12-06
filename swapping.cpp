4b260539daa5db8c01dd93d3e757c187abde74ed
Rejaul Karim Tirtho <rktirtho@gmail.com>
2020-12-06 11:24:23 +0600

    Sapping problem

---
+++ swapping.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+
+int leftHand, reghtHand, dolasHand;
+leftHand =50;
+reghtHand= 80;
+
+
+dolasHand = leftHand;
+leftHand = reghtHand;
+reghtHand = dolasHand;
+
+printf("Left Hand = %d  Right Hand = %d", leftHand, reghtHand);
+
+
+
+
+
+ return 0;
+}
